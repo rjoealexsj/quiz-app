5b4583feb66b697fa12050db4acec73efe8d7bfe
king-nelson-001 <arockianelson155@gmail.com>
2025-03-26 23:05:37 +0530

    data.js commit

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import '../Quiz/Quiz.css';
-
+import { data } from '../../Assets/data';
 const Quiz = () => {
+
+  let [index,setIndex] = useState(0);
+  let [question,setQuestion] = useState(data[index]);
+
+const checkAns =(e,ans) {
+  if(question.ans==ans) {
+    e.target.classList.add("correct");
+  }
+  else{
+    e.target.classList.add("wrong");
+  }
+}
+
   return (
     <div className='container'>
 <h1>Quiz App</h1>
-<hr/>
-<h2>which device is required for the internet connection?</h2>
+<hr />
+<h2>{index+1}. {question.question}</h2>
 <u1>
-    <li>Modem</li> 
-    <li>Router</li>
-    <li>LAN Cable</li>
-    <li>Pen Drive</li>
+    <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li> 
+    <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+    <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+    <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
 </u1>
 <button>Next</button>
-    <div  classname="index"> 1 of 5 questions</div></div>
+    <div  className="index"> 1 of 5 questions</div></div>
   )
 }
 
